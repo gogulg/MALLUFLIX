f3f39792e6855c30ddc8a1387354f214634da32a
gokul <gokulgalaxy47@gmail.com>
2022-05-31 12:35:00 +0530

    Formatted

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-const express = require('express')
+const express = require("express");
 const app = express();
 
-const path = require('path');
+const path = require("path");
 const port = process.env.PORT || 5000;
 
-if(process.env.NODE_ENV ==='production'){
-    app.use(express.static('build'));
-    app.get('*',(req,res)=>{
-        req.sendFile(path.resolve(__dirname,'build,index.html'))
-    })
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("build"));
+  app.get("*", (req, res) => {
+    req.sendFile(path.resolve(__dirname, "build,index.html"));
+  });
 }
 
-app.listen(port,(err)=>{
-    if(err) return console.log(err);
-    console.log("Server running on PORT",port);
-})
+app.listen(port, (err) => {
+  if (err) return console.log(err);
+  console.log("Server running on PORT", port);
+});
--- src/components/RowPost/RowPost.js
+++ src/components/RowPost/RowPost.js
@@ -1,71 +1,81 @@
-import React, { useEffect, useState } from 'react'
-import axios from '../../axios'
-import { API_KEY, imageURL } from '../../constant';
-import './RowPost.css'
-import YouTube, { YouTubeProps } from 'react-youtube';
+import React, { useEffect, useState } from "react";
+import axios from "../../axios";
+import { API_KEY, imageURL } from "../../constant";
+import "./RowPost.css";
+import YouTube, { YouTubeProps } from "react-youtube";
 
 function RowPost(props) {
+  const [video, setVideo] = useState("");
+  const [Posters, setPosters] = useState("");
 
-    const [video, setVideo] = useState('')
-    const [Posters, setPosters] = useState('')
+  const opts = {
+    height: "390",
+    width: "640",
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: 1,
+    },
+  };
+  const [state, setState] = useState([]);
+  useEffect(() => {
+    axios.get(props.url).then((response) => {
+      console.log(response.data.results);
+      setState(response.data.results);
+    });
+  }, []);
 
-    const opts = {
-        height: '390',
-        width: '640',
-        playerVars: {
-            // https://developers.google.com/youtube/player_parameters
-            autoplay: 1,
-        },
-    };
-    const [state, setState] = useState([])
-    useEffect(() => {
-        axios.get(props.url).then((response) => {
-            console.log(response.data.results);
-            setState(response.data.results)
-        })
-    }, [])
+  // const handleClick = (id) => {
+  //     console.log(id);
+  //     axios.get(`movie/${id}/videos?api_key=${API_KEY}&language=en-US`).then((response) => {
+  //         if (response.data.results.length[0] !== 0) {
+  //             setVideo(response.data.results[0])
+  //             console.log(response.data.results[0])
+  //         }
+  //     })
 
-    // const handleClick = (id) => {
-    //     console.log(id);
-    //     axios.get(`movie/${id}/videos?api_key=${API_KEY}&language=en-US`).then((response) => {
-    //         if (response.data.results.length[0] !== 0) {
-    //             setVideo(response.data.results[0])
-    //             console.log(response.data.results[0])
-    //         }
-    //     })
+  // }
 
-    // }
+  const handleClick = (id) => {
+    axios
+      .get(
+        `/movie/${id}/images?api_key=fa3f14157f9ffda08da13922ef5bf0d4&language=en-US&include_image_language=ml`
+      )
+      .then((response) => {
+        // console.log(response.data.posters[0]);
 
-    const handleClick = (id) => {
+        // setPosters(response.data.posters[i])
+        for (var i = 0; i < 2; i++) {
+          setPosters(response.data.posters);
+          // console.log(response.data.posters.length);
+          console.log("Images :", response.data.posters[i]);
+        }
+      });
+  };
 
-        axios.get(`/movie/${id}/images?api_key=fa3f14157f9ffda08da13922ef5bf0d4&language=en-US&include_image_language=ml`).then((response) => {
-            // console.log(response.data.posters[0]);
-
-            // setPosters(response.data.posters[i]) 
-            for (var i = 0; i < 2; i++) {
-                setPosters(response.data.posters)
-                // console.log(response.data.posters.length);
-                console.log("Images :", response.data.posters[i]);
-            }
-
-
-        })
-
-    }
-
-    return (
-        <div className='row'>
-            <h1>{props.title}</h1>
-            <div className='posters'>
-                {state.map((obj, index) => (
-                    <img onClick={() => handleClick(obj.id)} className={props.isSmall ? 'smallPoster' : 'poster'} src={`${imageURL + obj.backdrop_path}`} key={index}></img>
-                ))}
-            </div>
-            {/* {video && <YouTube videoId={video.key} opts={opts} />} */}
-            {Posters && Posters.map((obj, index) => (
-                <img className='relatedPosters' key={index} src={imageURL + obj.file_path}  ></img>))}
-        </div>
-    )
+  return (
+    <div className="row">
+      <h1>{props.title}</h1>
+      <div className="posters">
+        {state.map((obj, index) => (
+          <img
+            onClick={() => handleClick(obj.id)}
+            className={props.isSmall ? "smallPoster" : "poster"}
+            src={`${imageURL + obj.backdrop_path}`}
+            key={index}
+          ></img>
+        ))}
+      </div>
+      {/* {video && <YouTube videoId={video.key} opts={opts} />} */}
+      {Posters &&
+        Posters.map((obj, index) => (
+          <img
+            className="relatedPosters"
+            key={index}
+            src={imageURL + obj.file_path}
+          ></img>
+        ))}
+    </div>
+  );
 }
 
-export default RowPost
+export default RowPost;
